0d47f661682f9daed54926e4c40ab3ffc67051b7
Paras Dewangan <dewparas56@gmail.com>
2026-07-23 23:05:16 +0530

    Time: 25 ms (40.45%), Space: 23.3 MB (91.75%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -19,11 +19,11 @@ private:
             return;
         }
 
-        if(root -> val > q -> val){
+        if(root -> val > q -> val){ // node val is 8, q val is 4  
             solve(root ->left, p, q, res);
-        }else if(root -> val < p -> val ){
+        }else if(root -> val < p -> val ){ // node val 2, p val 7
             solve(root -> right, p, q, res);
-        }else{
+        }else{ // found the splitting point node val 4 , p val 3, q val 5
             res = root;
             return ;
         }
