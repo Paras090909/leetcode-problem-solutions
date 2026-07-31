b003ce70a52c81603612c9a9050a45211375f336
Paras Dewangan <dewparas56@gmail.com>
2026-07-31 23:45:12 +0530

    Time: 79 ms (10.27%), Space: 25.3 MB (94.44%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -14,30 +14,30 @@ public:
         }
         int res = 0;
         int i = 0;
-       while(i < 8){
+       while(i < 26){
+        if(i < 8){
             while(freq[i]--){
-            res+=1;
+                res+=1;
+            }
+            i++;
+        }else if(i < 16){
+             while(freq[i]--){
+                res+=2;
             }
             i++;
         }
-        while(i < 16){
-            while(freq[i]--){
-            res+=2;
+        else if (i < 24){
+             while(freq[i]--){
+                res+=3;
+            }
+            i++;
+        }else{
+             while(freq[i]--){
+                res+=4;
             }
             i++;
         }
-        while(i < 24){
-            while(freq[i]--){
-            res+=3;
-            }
-            i++;
-        }
-        while(i < 26){
-            while(freq[i]--){
-            res+=4;
-            }
-            i++;
-        }
+       }
 
         return res;
     }
