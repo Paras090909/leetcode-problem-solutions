c48322eb0197c5c85596267ce436439f91c966a5
Paras Dewangan <dewparas56@gmail.com>
2026-08-24 01:05:12 +0530

    Time: 0 ms (100%), Space: 146.7 MB (92.1%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -6,33 +6,33 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-
-private:
-    void solve(TreeNode* root, int &res, int &depth){
-        if(root == nullptr ) return;
-        depth++;
-        if(root->left == nullptr && root->right == nullptr){
-            if(depth < res){
-                res = depth;
-            }
-        }
-        
-        solve(root->left, res, depth);
-        solve(root->right, res, depth);
-        depth--;
-        return;
-    }
-
 public:
     int minDepth(TreeNode* root) {
-        if(root == nullptr) return 0;
-        int res = INT_MAX;
-        int depth = 0;
-        solve(root, res, depth);
-        return res;
+        if (root == nullptr)
+            return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int res = 0;
+        while (!q.empty()) {
+            res++;
+            int s = q.size();
+            while (s--) {
+                TreeNode* t = q.front();
+                q.pop();
+                if (!t->left && !t->right) {
+                    return res;
+                }
+                if (t->left != nullptr)
+                    q.push(t->left);
+                if (t->right != nullptr)
+                    q.push(t->right);
+            }
+        }
+        return 0;
     }
 };
